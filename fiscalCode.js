c49dad81dfc701ee2f62085ec7683e97bb9bc39d
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-09 07:38:37 -0800

    Begin to declare/initalize functions/variables.

--- fiscalCode.js
+++ fiscalCode.js
@@ -1,3 +1,11 @@
 const months = { 1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "H", 7: "L", 8: "M", 9: "P", 10: "R", 11: "S", 12: "T" };
 
-function fiscalCode(person) {}
+const fiscalCode = (person) => {
+  let name = person.name,
+    surname = person.surname,
+    gender = person.gender,
+    dob = person.dob;
+
+  const consonants = (str) => str.replace(/[aeiou]/g, ""),
+    vowels = (str) => str.replace(/[^aeiou]/g, "");
+};
