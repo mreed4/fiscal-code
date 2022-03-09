ea03e4870458e7c31fb2b79bfcea8d092cf3e941
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-09 08:13:20 -0800

    All variables added; Surname generator works

--- fiscalCode.js
+++ fiscalCode.js
@@ -4,8 +4,49 @@ const fiscalCode = (person) => {
   let name = person.name,
     surname = person.surname,
     gender = person.gender,
-    dob = person.dob;
+    dob = person.dob,
+    n = "",
+    s = "",
+    g = "",
+    d = "";
 
-  const consonants = (str) => str.replace(/[aeiou]/g, ""),
-    vowels = (str) => str.replace(/[^aeiou]/g, "");
+  const consonants = (str) => str.match(/[^aeiou]/g),
+    vowels = (str) => str.match(/[aeiou]/g);
+
+  let consonantsInName = consonants(name).length,
+    vowelsInName = vowels(name).length,
+    consonantsInSurname = consonants(surname).length,
+    vowelsInSurname = vowels(surname).length;
+
+  const partSurname = (consonantsInSurname) => {
+    let part =
+      surname.length < 3
+        ? [...surname].concat(["x"])
+        : consonantsInSurname >= 3
+        ? consonants(surname).slice(0, 3)
+        : consonantsInSurname < 3
+        ? consonants(surname).slice(0, 2).concat(vowels(surname).slice(0, 1))
+        : null;
+
+    return part;
+  };
+
+  n = partSurname(consonantsInSurname).join("").toUpperCase();
+  return n;
 };
+
+let test = {
+  name: "Matt",
+  surname: "Edabit",
+  gender: "M",
+  dob: "1/1/1900",
+};
+
+test = {
+  name: "Helen",
+  surname: "Fox",
+  gender: "F",
+  dob: "1/12/1950",
+};
+
+console.log(fiscalCode(test));
