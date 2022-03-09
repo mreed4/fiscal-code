cc000997ccf02915ba59286542d6ef7edff5511d
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-09 08:28:38 -0800

    Rname some functions and variables

--- fiscalCode.js
+++ fiscalCode.js
@@ -1,24 +1,24 @@
 const months = { 1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "H", 7: "L", 8: "M", 9: "P", 10: "R", 11: "S", 12: "T" };
 
-const fiscalCode = (person) => {
+const generateFiscalCode = (person) => {
   let name = person.name,
     surname = person.surname,
     gender = person.gender,
     dob = person.dob,
-    n = "",
-    s = "",
-    g = "",
-    d = "";
+    namePart,
+    surnamePart,
+    genderPart,
+    dobPart;
 
-  const consonants = (str) => str.match(/[^aeiou]/g),
-    vowels = (str) => str.match(/[aeiou]/g);
+  const consonants = (str) => str.match(/[^aeiou]/gi),
+    vowels = (str) => str.match(/[aeiou]/gi);
 
   let consonantsInName = consonants(name).length,
     vowelsInName = vowels(name).length,
     consonantsInSurname = consonants(surname).length,
     vowelsInSurname = vowels(surname).length;
 
-  const partSurname = (consonantsInSurname) => {
+  const generateSurnamePart = (consonantsInSurname) => {
     let part =
       surname.length < 3
         ? [...surname].concat(["x"])
@@ -26,13 +26,18 @@ const fiscalCode = (person) => {
         ? consonants(surname).slice(0, 3)
         : consonantsInSurname < 3
         ? consonants(surname).slice(0, 2).concat(vowels(surname).slice(0, 1))
-        : null;
+        : "XXX";
 
     return part;
   };
 
-  n = partSurname(consonantsInSurname).join("").toUpperCase();
-  return n;
+  surnamePart = generateSurnamePart(consonantsInSurname);
+
+  let parts = [surnamePart];
+
+  let fiscalCode = parts.map((part) => part.join("").toUpperCase()).join("");
+
+  return fiscalCode;
 };
 
 let test = {
@@ -42,11 +47,13 @@ let test = {
   dob: "1/1/1900",
 };
 
+/*
 test = {
   name: "Helen",
   surname: "Fox",
   gender: "F",
   dob: "1/12/1950",
 };
+*/
 
-console.log(fiscalCode(test));
+console.log(generateFiscalCode(test));
