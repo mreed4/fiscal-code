1a52eaa818bd84d22015c9069bc6a7e8b2b5e3b7
Matthew <78713317+mreed4@users.noreply.github.com>
2022-03-10 22:02:16 -0800

    Fix some logic in 'namePart' generator

--- fiscalCode.js
+++ fiscalCode.js
@@ -1,4 +1,4 @@
-const generateFiscalCode = (person) => {
+const fiscalCode = (person) => {
   const months = { 1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "H", 7: "L", 8: "M", 9: "P", 10: "R", 11: "S", 12: "T" };
 
   let name = person.name,
@@ -14,10 +14,8 @@ const generateFiscalCode = (person) => {
     vowels = (str) => str.match(/[aeiou]/gi);
 
   // The number of consonants and vowels in name and surname
-  let consonantsInName = consonants(name).length,
-    vowelsInName = vowels(name).length,
-    consonantsInSurname = consonants(surname).length,
-    vowelsInSurname = vowels(surname).length;
+  let consonantsInName = consonants(name).length;
+  consonantsInSurname = consonants(surname).length;
 
   const generateSurnamePart = (consonantsInSurname) => {
     let arr = consonants(surname);
@@ -37,7 +35,7 @@ const generateFiscalCode = (person) => {
     let arr = consonants(name);
     let part =
       name.length < 3
-        ? [...name].concat(["x"])
+        ? [...name].reverse().concat(["x"])
         : consonantsInName > 3
         ? [arr[0] + arr[2] + arr[3]]
         : consonantsInName === 3
@@ -50,11 +48,16 @@ const generateFiscalCode = (person) => {
   };
 
   const generateGenderAndDobPart = (gender, dob) => {
-    let [day, month, year] = dob;
+    let part,
+      [day, month, year] = dob;
+
     year = String(year).slice(2);
     month = months[month];
     day = gender === "F" ? day + 40 : gender === "M" && day < 10 ? 0 + String(day) : day;
-    return [year, month, day];
+
+    part = [year, month, day];
+
+    return part;
   };
 
   // Tie it all together
@@ -63,10 +66,10 @@ const generateFiscalCode = (person) => {
   genderAndDobPart = generateGenderAndDobPart(gender, dob);
 
   let parts = [surnamePart, namePart, genderAndDobPart];
-  let fiscalCode = parts
+  let final = parts
     .map((part) => part.join(""))
     .join("")
     .toUpperCase();
 
-  return fiscalCode;
+  return final;
 };
